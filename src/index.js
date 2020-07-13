98fb46590e2f434623b9cbecfce21256503df325
Yevhen <yevhen.code@gmail.com>
2020-07-13 13:44:06 +0300

    intended shared files

--- src/index.js
+++ src/index.js
@@ -1,35 +1,21 @@
 import {createStore} from 'redux';
-
-// функция редюсер
-const counter = (state = 0, action) => {
-    switch (action.type){
-        case 'INC':
-            return state + 1;
-        case 'DEC':
-            return state - 1;
-        case 'RES':
-            return 0;
-        default:
-            return state;
-    }
-}
+import counter from './reduser';
+import {inc, dec, res} from './actions';
 
 const store = createStore(counter);
+const {dispatch} = store;
 
-const inc = () => ({type: 'INC'});
-const dec = () => ({type: 'DEC'});
-const res = () => ({type: 'RES'});
 
 document.getElementById('inc').addEventListener('click', () => {
-    store.dispatch(inc())
+    dispatch(inc())
 });
 
 document.getElementById('dec').addEventListener('click', () => {
-    store.dispatch(dec())
+    dispatch(dec())
 });
 
 document.getElementById('res').addEventListener('click', () => {
-    store.dispatch(res())
+    dispatch(res())
 });
 
 //  функция обновляет counter
